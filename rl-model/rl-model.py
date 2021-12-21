29994e7eda4f1bb0c64ef4b826fc48db0a50f495
Benedict Oakes <sgboakes@liverpoool.ac.uk>
2021-12-21 12:55:06 +0000

    script for rl

--- rl-model/rl-model.py
+++ rl-model/rl-model.py
@@ -1,1 +1,360 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+import reverb
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import tensorflow as tf
+import numpy as np
+from numpy import int32, float32
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
+from tf_agents.trajectories import time_step as ts
+
+
+# ~~~~~~~~~~~~~~~ HYPERPARAMETERS ~~~~~~~~~~~~~~~
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 100  # @param {type:"integer"}
+collect_steps_per_iteration =   1# @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+# ~~~~~~~~~~~~~~~ ENVIRONMENT ~~~~~~~~~~~~~~~
+
+class PyEnvironment(object):
+
+    def reset(self):
+        """Return initial_time_step."""
+        self._current_time_step = self._reset()
+        return self._current_time_step
+
+    def step(self, action):
+        """Apply action and return new time_step."""
+        if self._current_time_step is None:
+            return self.reset()
+        self._current_time_step = self._step(action)
+        return self._current_time_step
+
+    def current_time_step(self):
+        return self._current_time_step
+
+    def time_step_spec(self):
+        """Return time_step_spec."""
+
+    @abc.abstractmethod
+    def observation_spec(self):
+        """Return observation_spec."""
+
+    @abc.abstractmethod
+    def action_spec(self):
+        """Return action_spec."""
+
+    @abc.abstractmethod
+    def _reset(self):
+        """Return initial_time_step."""
+
+    @abc.abstractmethod
+    def _step(self, action):
+        """Apply action and return new time_step."""
+
+
+class SatEnv(py_environment.PyEnvironment):
+
+    def __init__(self):
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(), dtype=np.int32, minimum=0, maximum=10, name='action')
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(1,), dtype=np.int32, minimum=0, name='observation')
+        self._state = 0
+        self._episode_ended = False
+        self._episode_duration = 0
+        self._max_episode_length = 20
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def _reset(self):
+        # self._state = 0
+        self._episode_ended = False
+        return ts.restart(np.array([self._state], dtype=np.int32))
+
+    def _step(self, action):
+
+        reward = 0
+        if self._episode_ended:
+            # The last action ended the episode. Ignore the current action and start
+            # a new episode.
+            return self.reset()
+
+        self._episode_duration += 1
+
+        # Make sure episodes don't go on forever.
+        if action == self._state:
+            reward = 1
+
+        self._state = (self._state + 1) % 10
+        # print(self._state)
+        # print("reward = ", reward)
+
+        if self._episode_duration >= self._max_episode_length:
+            self._episode_ended = True
+            return ts.termination(np.array([self._state], dtype=np.int32), reward)
+        else:
+            return ts.transition(
+                np.array([self._state], dtype=np.int32), reward=reward, discount=1.0)
+
+
+env = SatEnv()
+utils.validate_py_environment(env, episodes=10)
+
+tf_env = tf_py_environment.TFPyEnvironment(env)
+time_step = tf_env.reset()
+rewards = []
+steps = []
+num_episodes = 5
+
+for _ in range(num_episodes):
+    episode_reward = 0
+    episode_steps = 0
+    while not time_step.is_last():
+        action = tf.random.uniform([1], 0, 2, dtype=tf.int32)
+        time_step = tf_env.step(action)
+        episode_steps += 1
+        episode_reward += time_step.reward.numpy()
+    rewards.append(episode_reward)
+    steps.append(episode_steps)
+    time_step = tf_env.reset()
+
+num_steps = np.sum(steps)
+avg_length = np.mean(steps)
+avg_reward = np.mean(rewards)
+
+print('num_episodes:', num_episodes, 'num_steps:', num_steps)
+print('avg_length', avg_length, 'avg_reward:', avg_reward)
+
+env.reset()
+
+train_py_env = env
+eval_py_env = env
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+
+# ~~~~~~~~~~~~~~~ AGENT ~~~~~~~~~~~~~~~
+
+fc_layer_params = (100, 50)
+action_tensor_spec = tensor_spec.from_spec(env.action_spec())
+num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+# Define a helper function to create Dense layers configured with the right
+# activation and kernel initializer.
+def dense_layer(num_units):
+  return tf.keras.layers.Dense(
+      num_units,
+      activation=tf.keras.activations.relu,
+      kernel_initializer=tf.keras.initializers.VarianceScaling(
+          scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+# QNetwork consists of a sequence of Dense layers followed by a dense layer
+# with `num_actions` units to generate one q_value per available action as
+# its output.
+dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+q_values_layer = tf.keras.layers.Dense(
+    num_actions,
+    activation=None,
+    kernel_initializer=tf.keras.initializers.RandomUniform(
+        minval=-0.03, maxval=0.03),
+    bias_initializer=tf.keras.initializers.Constant(-0.2))
+q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+eval_policy = agent.policy
+collect_policy = agent.collect_policy
+
+random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                train_env.action_spec())
+
+# ~~~~~~~~~~~~~~~ METRICS AND EVALUATION ~~~~~~~~~~~~~~~
+
+
+def compute_avg_return(environment, policy, num_episodes=10):
+
+    total_return = 0.0
+    for _ in range(num_episodes):
+
+        time_step = environment.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+        total_return += episode_return
+
+    avg_return = total_return / num_episodes
+    return avg_return.numpy()[0]
+
+
+compute_avg_return(eval_env, random_policy, num_eval_episodes)
+
+# ~~~~~~~~~~~~~~~ REPLAY BUFFER ~~~~~~~~~~~~~~~
+
+table_name = 'uniform_table'
+replay_buffer_signature = tensor_spec.from_spec(
+      agent.collect_data_spec)
+replay_buffer_signature = tensor_spec.add_outer_dim(
+    replay_buffer_signature)
+
+table = reverb.Table(
+    table_name,
+    max_size=replay_buffer_max_length,
+    sampler=reverb.selectors.Uniform(),
+    remover=reverb.selectors.Fifo(),
+    rate_limiter=reverb.rate_limiters.MinSize(1),
+    signature=replay_buffer_signature)
+
+reverb_server = reverb.Server([table])
+
+replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
+    agent.collect_data_spec,
+    table_name=table_name,
+    sequence_length=2,
+    local_server=reverb_server)
+
+rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+  replay_buffer.py_client,
+  table_name,
+  sequence_length=2)
+
+agent.collect_data_spec
+agent.collect_data_spec._fields
+
+# ~~~~~~~~~~~~~~~ DATA COLLECTION ~~~~~~~~~~~~~~~
+
+py_driver.PyDriver(
+    env,
+    py_tf_eager_policy.PyTFEagerPolicy(
+      random_policy, use_tf_function=True),
+    [rb_observer],
+    max_steps=initial_collect_steps).run(train_py_env.reset())
+
+dataset = replay_buffer.as_dataset(
+    num_parallel_calls=3,
+    sample_batch_size=batch_size,
+    num_steps=2).prefetch(3)
+
+iterator = iter(dataset)
+
+# ~~~~~~~~~~~~~~~ TRAINING AGENT ~~~~~~~~~~~~~~~
+
+# try:
+#     %%time
+# except:
+#     pass
+
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+agent.train = common.function(agent.train)
+
+# Reset the train step.
+agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+# Reset the environment.
+time_step = train_py_env.reset()
+
+# Create a driver to collect experience.
+collect_driver = py_driver.PyDriver(
+    env,
+    py_tf_eager_policy.PyTFEagerPolicy(
+      agent.collect_policy, use_tf_function=True),
+    [rb_observer],
+    max_steps=collect_steps_per_iteration)
+
+for _ in range(num_iterations):
+
+    # Collect a few steps and save to the replay buffer.
+    time_step, _ = collect_driver.run(time_step)
+
+    # Sample a batch of data from the buffer and update the agent's network.
+    experience, unused_info = next(iterator)
+    train_loss = agent.train(experience).loss
+
+    step = agent.train_step_counter.numpy()
+
+    if step % log_interval == 0:
+        print('step = {0}: loss = {1}'.format(step, train_loss))
+
+    if step % eval_interval == 0:
+        avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+        print('step = {0}: Average Return = {1}'.format(step, avg_return))
+        returns.append(avg_return)
+
+# ~~~~~~~~~~~~~~~ VISUALISATION ~~~~~~~~~~~~~~~
+
+iterations = range(0, num_iterations + 1, eval_interval)
+plt.plot(iterations, returns)
+plt.ylabel('Average Return')
+plt.xlabel('Iterations')
